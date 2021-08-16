a373279f3167006f88b5dcdc97656a9694df1026
FedorPlechov <plechov.fedor@gmail.com>
2021-08-16 17:48:47 +0300

    try 3

--- src/components/layouts/TheListOfProjects.vue
+++ src/components/layouts/TheListOfProjects.vue
@@ -89,13 +89,13 @@ export default {
       el.style.transform = 'scale(1)';
     }
   },
-  mounted() {
-   const blocks = document.getElementsByClassName('li>div:first-of-type')
-    blocks.forEach(el => el.addEventListener('touchend', (event)=>{
-      event.target.dispatchEvent('mouseenter')
-      console.log(123)
-    }))
-  }
+  // mounted() {
+  //  const blocks = document.getElementsByClassName('li>div:first-of-type')
+  //   blocks.forEach(el => el.addEventListener('touchend', (event)=>{
+  //     event.target.dispatchEvent('mouseenter')
+  //
+  //   }))
+  // }
 }
 </script>
 
